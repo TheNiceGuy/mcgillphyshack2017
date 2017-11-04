0220b915ef59d671d3f01955ffbdb69355212096
Félix Desrochers <felix.desrochers@polymtl.ca>
2017-11-04 17:48:46 -0400

    ajustement de map.py

--- src/constants.py
+++ src/constants.py
@@ -5,5 +5,10 @@ physical constants to be used
 @author: utilisateur1
 """
 
+import numpy as np
+
 #gravitationnal constant
 G = 6.6740831*10**(-11)
+masse_t = 5.9722*(10)**24
+radius_t = 6378.137 *(10)**3
+density_t = (masse_t)/((4*np.pi*radius_t**3)/3)
--- src/map.py
+++ src/map.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
+
+#Importing packages
 from celestial_object import*
-#from constants import*
+from constants import*
 from planet import*
-#from potential import*
-#from rocket import*
+from potential import*
+from rocket import*
 import numpy as np
+
+#Defining constants
+dt = 1
 x=150*10**6*np.random.rand(6)+10**8
 y=150*10**6*np.random.rand(6)+10**8
 vx=30000*np.random.rand(6)+10**3
 vy=30000*np.random.rand(6)+10**3
 mass=5*10**28*np.random.rand(6)+4.5*10**27
-radius=6000000*np.random.rand(6)+9*10**5
+radius = [(((3*m)/(densitee_terre * 4 * np.pi))**(1/3))/150 for m in mass]
 w=7.2921159*10**-5*np.random.randn(6)+1.09367*10**-5
+
+#Constructing the objectList dictionnary
 objectList={}
-for i in range(0,6):
-    p=Planet(x[i],y[i],vx[i],vy[i],mass[i],radius[i],w[i])
-    objectList.update({p.index:p})
-#print('ax: {}'.format(objectList[1].ax))
+for i in range(CelestialObject.count):
+    objectList.update({i:Planet(x[i],y[i],vx[i],vy[i],mass[i],radius[i],w[i])})
 
-#print('x: {}'.format(objectList[1].x))
+def Step():
+    #Update the acceleration for all objects
+    for planet in objectList.values():
+        planet.acceleration(objectList)
 
-for planet in objectList.values():
-    planet.acceleration(objectList)
-print(objectList[1].ax)
-for planet in objectList.values():
-    planet.actualizeSystem(2)
+    #Update the data of all the objects
+    for planet in objectList.values():
+        planet.actualizeSystem(dt)
 
-#print('ax: {}'.format(objectList[1].ax))
 
-#print('x: {}'.format(objectList[1].x))
