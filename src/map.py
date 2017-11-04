773537b23e375d241f2441ba58aca658a9cfb4bf
Félix Desrochers <felix.desrochers@polymtl.ca>
2017-11-04 17:58:09 -0400

    Réajustement de map.py

--- src/map.py
+++ src/map.py
@@ -18,18 +18,27 @@ mass=5*10**28*np.random.rand(6)+4.5*10**27
 radius = [(((3*m)/(densitee_terre * 4 * np.pi))**(1/3))/150 for m in mass]
 w=7.2921159*10**-5*np.random.randn(6)+1.09367*10**-5
 
-#Constructing the objectList dictionnary
-objectList={}
-for i in range(CelestialObject.count):
-    objectList.update({i:Planet(x[i],y[i],vx[i],vy[i],mass[i],radius[i],w[i])})
 
-def Step():
-    #Update the acceleration for all objects
-    for planet in objectList.values():
-        planet.acceleration(objectList)
+class map():
 
-    #Update the data of all the objects
-    for planet in objectList.values():
-        planet.actualizeSystem(dt)
+    def __init__(self):
+        #Constructing the objectList dictionnary
+        self.objectList={}
+        for i in range(CelestialObject.count):
+            self.objectList.update({i:Planet(x[i],y[i],vx[i],vy[i],mass[i],radius[i],w[i])})
 
+    #Step function
+    def Step(self):
+        #Update the acceleration for all objects
+        for objetSpatial in objectList.values():
+            objetSpatial.acceleration(objectList)
+
+        #Update the data of all the objects
+        for objetSpatial in objectList.values():
+            #Actualize the position
+            objetSpatial.actualizeSystem(dt)
+
+            #If the object is a Planet update the angle
+            if isinstance(planet,Planet):
+                objetSpatial.actualizeAngle(dt)
 
