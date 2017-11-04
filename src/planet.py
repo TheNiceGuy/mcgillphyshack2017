450aacddad5cda9c0cea23f94706d1624d2bc9f1
Benoit Brizard <brizardbenoit@gmail.com>
2017-11-04 15:09:30 -0400

    steady planet

--- src/planet.py
+++ src/planet.py
@@ -12,3 +12,18 @@ class Planet(CelestialObject):
         self.angle=self.w*dt+self.angle
     def getAngle(self):
         return self.angle
+
+class SteadyPlanet(Planet):
+    def __init__(self,x,y,vx,vy,mass,radius,w):
+        super().__init__(x,y,vx,vy,mass,radius,w)
+
+    def distance(self, other_object):
+        pass
+    def acceleration(self,objectList,dt):
+        pass
+    def  actualizeSystem(self,dt):
+        pass
+
+a=SteadyPlanet(1,2,3,4,5,6,7)
+a.actualizeSystem(5)
+print(a.x)
