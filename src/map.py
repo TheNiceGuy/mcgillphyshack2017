4a2ff79caba4ab87b90ec2227554714cbd670422
Benoit Brizard <brizardbenoit@gmail.com>
2017-11-04 17:27:51 -0400

    first map ever

--- src/map.py
+++ src/map.py
@@ -1,7 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  4 10:08:02 2017
+#!/usr/bin/env python3
+from celestial_object import*
+#from constants import*
+from planet import*
+#from potential import*
+#from rocket import*
+import numpy as np
+x=150*10**6*np.random.rand(6)+10**8
+y=150*10**6*np.random.rand(6)+10**8
+vx=30000*np.random.rand(6)+10**3
+vy=30000*np.random.rand(6)+10**3
+mass=5*10**28*np.random.rand(6)+4.5*10**27
+radius=6000000*np.random.rand(6)+9*10**5
+w=7.2921159*10**-5*np.random.randn(6)+1.09367*10**-5
+objectList={}
+for i in range(0,6):
+    p=Planet(x[i],y[i],vx[i],vy[i],mass[i],radius[i],w[i])
+    objectList.update({p.index:p})
+#print('ax: {}'.format(objectList[1].ax))
 
-@author: utilisateur1
-"""
+#print('x: {}'.format(objectList[1].x))
 
+for planet in objectList.values():
+    planet.acceleration(objectList)
+print(objectList[1].ax)
+for planet in objectList.values():
+    planet.actualizeSystem(2)
+
+#print('ax: {}'.format(objectList[1].ax))
+
+#print('x: {}'.format(objectList[1].x))
