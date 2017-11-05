c441e44eff44658849f0d940a0a661dd798909a9
CamaradeBastien <bast.guit22@hotmail.ca>
2017-11-05 07:51:11 -0500

    potential update en temps réel, et a un range vmin-vmax fixé au début de 0 à 1*10^-11

--- src/interface.py
+++ src/interface.py
@@ -116,7 +116,7 @@ class MainWindow(QWidget):
 
         self.__count += 1
 
-        if self.__count == 180:
+        if self.__count == 20:
             print("render map")
             (minx,maxx) = self.__game.getMap().getLimitsX()
             (miny,maxy) = self.__game.getMap().getLimitsY()
@@ -367,6 +367,6 @@ class PlotWidget(FigureCanvasQTAgg):
 
         self.__potential.compute(self.__map.getList(), minx, maxx, miny, maxy)
         self.__potential.actualisePlot()
- #       self.__figure.canvas.draw()
+        #self.__figure.canvas.draw()
         self.draw()
 
