27ee07d961f51506d9be62abb1170312bf17a711
CamaradeBastien <bast.guit22@hotmail.ca>
2017-11-05 10:04:24 -0500

    modification de system_moons et test de celui-ci (non-fonctionnel)

--- src/game.py
+++ src/game.py
@@ -6,9 +6,9 @@ import time
 
 from PyQt4 import QtGui
 from interface import *
-from liste_plan import *
+#from liste_plan import *
 #from systeme_solaire import *
-#from system_moons import *
+from system_moons import *
 
 from map import *
 
