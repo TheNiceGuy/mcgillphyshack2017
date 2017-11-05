5621d49b8c734ed1f3b524e219c686ff8720894f
CamaradeBastien <bast.guit22@hotmail.ca>
2017-11-05 09:21:55 -0500

    creation d'un autre systeme, des lunes autour d'une planete

---
+++ src/system_moons.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 08:52:06 2017
+
+@author: utilisateur1
+"""
+#Importer les modules
+from celestial_object import*
+from constants import*
+from planet import*
+from potential import*
+from rocket import*
+import numpy as np
+import math
+
+objectList={}
+#Definition des planètes du système solaire
+objectList.update({0:Planet(0            ,0    ,0              ,0              ,1.5*10**24              ,500*10**6 , 10)})
+objectList.update({1:Planet( 4*10**6*math.cos(math.pi/4)      ,4*10**6*math.sin(math.pi/4)    ,1000*math.cos(math.pi/4)              ,1000*math.sin(math.pi/4)          ,1,5*10**22              ,2*10**6  , 2)})
+objectList.update({2:Planet( 5*10**6*math.cos(math.pi/6)      ,5*10**6*math.sin(math.pi/6)    ,1200*math.cos(math.pi/6)              ,1200*math.sin(math.pi/6)          ,1,6*10**22              ,2.3*10**6  , 1)})
+objectList.update({3:Planet( 7*10**6*math.cos(math.pi/2)      ,7*10**6*math.sin(math.pi/2)    ,1500*math.cos(math.pi/2)              ,1500*math.sin(math.pi/2)          ,1,2*10**21              ,1.7*10**6  , 5)})
+objectList.update({5:Planet( 7.2*10**6    ,0    ,0              ,1550         ,1.8*10**21              ,1.8*10**6 , 12)})
+
+def getPlan(count):
+    rock=[]
+    return objectList, rock
