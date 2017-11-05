106a6de70b3d51faf9309d8d69f6e4c836fd514c
Benoit Brizard <brizardbenoit@gmail.com>
2017-11-05 02:18:10 -0500

    ajout rocket

--- src/rocket.py
+++ src/rocket.py
@@ -7,16 +7,23 @@ Created on Sat Nov  4 10:10:16 2017
 from constants import *
 from celestial_object import *
 import numpy as np
+from systeme_solaire import *
 
 class Rocket(CelestialObject):
     '''defining the rocket class that caracterizes the rocket in the simulation'''
 
-    def __init__(self, x, y, vx, vy, mass, radius, grounded=True, Parent_index=0, qte_gas=200*1000, ejection_speed=4000, mass_flow=1000, propulsion=False):
+    def __init__(self, x, y, vx, vy, mass, radius, Parent_index,theta0 ,grounded=True,  qte_gas=200*1000, ejection_speed=4000, mass_flow=1000, propulsion=False):
         super().__init__(x, y, vx, vy, mass, radius)
         self.qte_gas = qte_gas
         self.ejection_speed = ejection_speed
         self.mass_flow = mass_flow
+        self.Parent_index=Parent_index
+        self.grounded=grounded
+        self.propulsion=propulsion
+        self.x=objectList[Parent_index].x+objectList[Parent_index].radius*np.cos(theta0)
+        self.y=objectList[Parent_index].y+ objectList[Parent_index].radius*np.sin(theta0)
 
+        self.theta0=theta0
     def getGrounded(self):
         return self.grounded
 
@@ -43,14 +50,13 @@ class Rocket(CelestialObject):
 
         #Compute the gravitationnal acceleration as usual
         for celest_object in objectList.values():
-            if celest_object is not self:
-                self.ax += ((G * celest_object.mass)/self.distance(celest_object)**2) * ((celest_objects.x - self.x)/self.distance(celest_object))
-                self.ay += ((G * celest_object.mass)/self.distance(celest_object)**2) * ((celest_objects.y - self.y)/self.distance(celest_object))
+            self.ax += ((G * celest_object.mass)/self.distance(celest_object)**2) * ((celest_objects.x - self.x)/self.distance(celest_object))
+            self.ay += ((G * celest_object.mass)/self.distance(celest_object)**2) * ((celest_objects.y - self.y)/self.distance(celest_object))
 
         #If the propulsion is on, compute the acceleration of the propulsion
         if self.propulsion:
-            self.ax += - mass_flow * ( ejection_speed * (self.vx/np.sqrt(self.vx**2 + self.vy**2)) - self.vx )
-            self.ay += - mass_flow * ( ejection_speed * (self.vy/np.sqrt(self.vx**2 + self.vy**2)) - self.vy )
+            self.ax += - mass_flow * ( -ejection_speed * (self.vx/np.sqrt(self.vx**2 + self.vy**2)) - self.vx )
+            self.ay += - mass_flow * ( -ejection_speed * (self.vy/np.sqrt(self.vx**2 + self.vy**2)) - self.vy )
 
     def actualizeSystem(self, objectList, dt):
         #If it is grounded make it rotate on the surface of its mother planet
--- src/systeme_solaire.py
+++ src/systeme_solaire.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 #Importer les modules
 from celestial_object import*
 from constants import*
@@ -10,10 +8,10 @@ import numpy as np
 
 
 #Definition des planètes du système solaire
-soleil =  Planet(0            ,0    ,0              ,0              ,1.981*10**30              ,300*696342000 , 10)
-mercure = Planet(46001272000  ,0    ,0              ,58980          ,3.3011*10**23             ,1000*2439700  , 20)
-venus =   Planet(107476259000 ,0    ,0              ,35260          ,4.8685*10**24             ,1000*6051800  , 30)
-terre =   Planet(147098079000 ,0    ,0              ,30287          ,5.9736*10**24             ,1000*6371008  , 40)
+soleil =  Planet(0            ,0    ,0              ,0              ,1.981*10**30              ,50*696342000 , 10)
+mercure = Planet(46001272000  ,0    ,0              ,58980          ,3.3011*10**23             ,200*2439700  , 20)
+venus =   Planet(107476259000 ,0    ,0              ,35260          ,4.8685*10**24             ,200*6051800  , 30)
+terre =   Planet(147098079000 ,0    ,0              ,30287          ,5.9736*10**24             ,200*6371008  , 40)
 #mars  =   Planet(206644545000 ,0    ,0              ,26499          ,641.85*10**21             ,1000*3396200  , 15)
 #jupiter = Planet(740520000000 ,0    ,0              ,13720          ,1.8986*10**27             ,1000*69911000 , 12)
 #uranus  = Planet(2734998229000,0    ,0              ,7128           ,8.681 *10**25             ,1000*2559000  , 18)
@@ -24,7 +22,7 @@ def getPlan(count):
     dt=1
     objectList={}
     for i,planet in zip(range(4),[soleil,mercure,venus,terre]):#,mars,jupiter,uranus,neptune]):
-        print("allo")        
+        print("allo")
         objectList.update({i:planet})
     print(objectList)
     return objectList
